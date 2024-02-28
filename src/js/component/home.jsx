4a215a4469b1cf7ebbeb4a9fd5624594e331cda1
Anibal-JPG <109487916+Anibal-JPG@users.noreply.github.com>
2024-02-28 01:19:52 +0000

    test proyect n-1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
 import React from "react";
+import { useState } from "react";
+import InitialTask from "./tasks.json";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [searchValue, setSearchValue] = useState("");
+	const [tareas, setTareas] = useState(InitialTask);
+	const [hover, setHover] = useState(false)
+
+
+	return <>
+		<h1 className="title text-center mt-5">Todos</h1>
+		<Card className="d-flex" body>
+			<div className="row m-3">
+				<input className="inputEdit col-12" onKeyUp={(evt) => {
+					if (evt.key == 'Enter') {
+						setTareas([...tareas, { task: evt.target.value }], evt.target.value = "")
+					} else {
+						setSearchValue(evt.target.value)
+					}
+				}}
+					type="text" placeholder="whats need to be done?" defaultValue={searchValue}
+				/>
+			</div>
+			{tareas.map((task, ind) => {
+				return (
+					<div onMouseEnter={() => setHover(ind)} className = "row m-2" >
+						<h5 className="col-11 text-secondary" key={ind}>{task.task}</h5>
+						{hover == ind && <button className="botonEdit col-1" onClick={() => setTareas(tareas.filter((obj, index) => ind != index))} >x</button>}
+					</div>
+				)
+			})}
+
+		<div>
+			<span>{tareas.length} Item left</span>
 		</div>
-	);
+	</Card >
+
+		</>
 };
 
 export default Home;
